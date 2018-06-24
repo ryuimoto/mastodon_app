7bbda59fad773b75932a562042441b120e2c6c29
ryuimoto <narrow617@gmail.com>
2018-06-24 13:50:31 +0000

    データベースの適応

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_17_130517) do
+ActiveRecord::Schema.define(version: 2018_06_24_134321) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_comments_on_topic_id"
+  end
 
   create_table "mastodon_clients", force: :cascade do |t|
     t.string "domain"
