1d5054a089208fe1c59abc08190048229c6efeb3
ryuimoto <narrow617@gmail.com>
2018-06-24 13:45:27 +0000

    コメントモデルの追加

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+    before_action :set_topic
+
+    def create
+        comment = @topic.comments.create! comments_params
+
+        redirect_to @topic
+    end
+
+    private
+        def set_topic
+            @topic = Topic.find(params[:topic_id])
+        end
+
+        def comments_params
+            params.required(:comment).permit(:comment)
+        end
+end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,3 @@
 class Topic < ApplicationRecord
+    has many :comments
 end
