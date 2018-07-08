da53dd9c1e94d84e5bac18c6a121792cd222c489
ryuimoto <narrow617@gmail.com>
2018-07-08 12:51:56 +0000

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
     resources :comments 
   end
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root 'mastodon_login#index'
-  get 'mastodon_login/index'
+  root 'topics#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
