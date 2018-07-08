5b5bf39bc8151d880ca87aca641260adaad2d324
ryuimoto <narrow617@gmail.com>
2018-07-08 13:50:55 +0000

    fix layout mastodon_login#index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
     resources :comments 
   end
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root 'topics#index'
+  root 'mastodon_login#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
