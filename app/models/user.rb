15c24e63d61a2d00cb3cce103024fbc4be194da6
ryuimoto <narrow617@gmail.com>
2018-07-15 12:39:07 +0000

    using avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,20 @@ class User < ApplicationRecord
          def self.find_for_oauth(auth)
           user = User.where(email: auth.uid, provider: auth.provider).first
           
-          user.update!(:uid => auth.extra.raw_info.display_name)
+          if user != nil then
+            user.update!(:uid => auth.extra.raw_info.display_name)
+          end
+          
+          _, domain = auth.uid.split('@')
+          client = Mastodon::REST::Client.new(base_url: "https://#{domain}", bearer_token: auth.credentials.token)
       
           unless user
             user = User.create(
               uid:      auth.extra.raw_info.display_name,
               provider: auth.provider,
               email:    User.dummy_email(auth),
-              password: Devise.friendly_token[0, 20]
+              password: Devise.friendly_token[0, 20],
+              avatar:   client.verify_credentials.avatar
             )
             user.save!
           end
